f95c0a3bffe032b1be24a8f6207e5d55125557e3
David Amian <damian@emergya.com>
2014-05-10 12:39:56 +0200

    Added users oahi plugins

---
+++ files/default/plugins/users.rb
@@ -0,0 +1,30 @@
+provides 'ohai_gecos'
+
+if ohai_gecos.nil?
+  ohai_gecos Mash.new
+end
+
+require 'etc'
+
+users Array.new
+
+# LikeWise create the user homes at /home/local/DOMAIN/
+homedirs = Dir["/home/*"] + Dir["/home/local/*/*"]
+homedirs.each do |homedir|
+  temp=homedir.split('/')
+  user=temp[temp.size()-1]
+  begin
+    entry=Etc.getpwnam(user)
+    users << Mash.new(
+      :username => entry.name,
+      :home     => entry.dir,
+      :gid      => entry.gid,
+      :uid      => entry.uid
+    )
+  rescue Exception => e
+    puts 'User ' + user + ' doesn\'t exists'
+  end
+end
+
+ohai_gecos['users'] = users
+
