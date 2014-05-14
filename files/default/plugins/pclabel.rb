42259a82167a5f6b47f8548610f4e9cf2ffce7ee
David Amian <damian@emergya.com>
2014-05-14 16:57:30 +0200

    Added begin-rescue block

--- files/default/plugins/pclabel.rb
+++ files/default/plugins/pclabel.rb
@@ -3,11 +3,13 @@ provides 'ohai_gecos'
 if ohai_gecos.nil?
   ohai_gecos Mash.new
 end
-
-pclabel = ''
-File.open('/etc/pclabel','r') do |pclabelfile|
-  pclabel = pclabelfile.gets
+begin
+  pclabel = ''
+  File.open('/etc/pclabel','r') do |pclabelfile|
+    pclabel = pclabelfile.gets
+  end
+rescue Exception => e
+  puts e.message
 end
-
 
 ohai_gecos['pclabel'] = pclabel
