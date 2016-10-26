39c612fc554608eaee57ce9ac7d14c70840e18e2
Diego Martinez Castaneda <dmartinez@solutia-it.es>
2016-10-26 15:55:20 +0200

    added OpPolicy info

--- files/default/plugins/printers.rb
+++ files/default/plugins/printers.rb
@@ -4,6 +4,41 @@ if ohai_gecos.nil?
   ohai_gecos Mash.new
 end
 
+def getTagValue(ptr,tag)
+	# this function needs two parameters:
+	#  - ptr: name of the printer to be processed
+	#  - tag: tag to be extracted
+	# It returns two possible values:
+	#  - the value of tag
+	#  - false in case tag doesn't exist in ptr declaration
+
+	printerFound = false
+
+	begin
+		fp = File.open('/etc/cups/printers.conf', 'r')
+		f_printers = fp.read
+		fp.close
+	rescue
+		puts "file doesn't exist"
+		return false
+	end
+
+	if not f_printers.empty?
+		f_printers.each_line do |line|
+			if line.match(/^<Printer #{Regexp.escape ptr}>$/)
+				printerFound = true
+				next
+			end
+			if line.match(/^#{Regexp.escape tag} (.*)$/) and printerFound
+				return $1
+			end
+			if line.match(/^<\/Printer>$/) and printerFound
+				return false
+			end
+		end
+	end
+end
+
 begin
   entries = []
   printers = []
@@ -25,6 +60,9 @@ begin
             thisprinter Mash.new
             thisprinter[:status] = printerStatus
             thisprinter[:queue]  = printerQueue
+            puts printer.gsub("+","\+")
+            puts printer.gsub("+","\\+")
+            thisprinter[:oppolicy] = getTagValue(printer, 'OpPolicy')
             p = lpo.stdout.scan(/(?:\S+='.+?')|(?:\S+=\S+)/)
             p.each { |x| thisprinter[x.split(/=(.*)/)[0]] = x.split(/=(.*)/)[1]}
             printers << thisprinter
