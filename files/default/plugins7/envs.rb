90953627d090b521ae5e8c9b5a5379ebeaa988a9
root <root@gccserver.(none)>
2017-04-18 12:41:13 +0200

    System-wide environment variables: /etc/environment

---
+++ files/default/plugins7/envs.rb
@@ -0,0 +1,24 @@
+Ohai.plugin(:Envs) do
+
+    provides 'ohai_gecos'
+
+    collect_data do
+
+        if ohai_gecos.nil?
+          ohai_gecos Mash.new
+        end
+        begin
+          envvars = ''
+          fp = File.open('/etc/environment','r')
+          envvars = fp.read
+          fp.close
+
+          envs = Hash[envvars.each_line.map { |l| l.gsub(/(?:")*?([^"]+)(?:")*?/, '\1').chomp.split('=',2)}]
+  
+        rescue Exception => e
+          puts e.message
+        end
+
+        ohai_gecos['envs'] = envs
+    end
+end
---
+++ files/default/plugins7/proxy.rb
@@ -0,0 +1,40 @@
+Ohai.plugin(:Proxy) do
+
+    provides 'ohai_gecos'
+
+    collect_data do
+
+        if ohai_gecos.nil?
+          ohai_gecos Mash.new
+        end
+
+        require 'uri'
+
+        begin
+  
+          fp = File.open('/etc/environment', 'r')
+          envvars = fp.read
+          fp.close
+  
+          http_proxy  = envvars.scan(/http_proxy=(?:")*?([^"]+)(?:")*?/i).flatten.pop  || ENV['HTTP_PROXY']  || ENV['http_proxy']  || '' 
+          https_proxy = envvars.scan(/https_proxy=(?:")*?([^"]+)(?:")*?/i).flatten.pop || ENV['HTTPS_PROXY'] || ENV['https_proxy'] || ''
+   
+          http_proxy_host  = URI.parse(http_proxy.chomp).host  
+          http_proxy_port  = URI.parse(http_proxy.chomp).port  || 80
+          https_proxy_host = URI.parse(https_proxy.chomp).host 
+          https_proxy_port = URI.parse(https_proxy.chomp).port || 443
+
+          proxy = Mash.new(
+              :http_proxy_host  => http_proxy_host,
+              :http_proxy_port  => http_proxy_port,
+              :https_proxy_host => https_proxy_host,
+              :https_proxy_port => https_proxy_port
+          )
+
+        rescue Exception => e
+          puts "Proxy not found\n" + e.message
+        end
+
+        ohai_gecos['proxy'] = proxy
+    end
+end
